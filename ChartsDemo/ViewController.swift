eb09bf93dcb627932f7b1cbbdf32a9cf4f6b4c44
Aleksandar Angelov <axelsyrex@gmail.com>
2018-10-25 22:44:11 +0300

    Added: improvements

--- ChartsDemo/ViewController.swift
+++ ChartsDemo/ViewController.swift
@@ -26,48 +26,33 @@ class ViewController: UIViewController {
         chartView.gridBackgroundColor = UIColor.blue
         chartView.doubleTapToZoomEnabled = false
         chartView.highlightPerTapEnabled = true
-        
-        // x-axis limit line
-        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
-        llXAxis.lineWidth = 4
-        llXAxis.lineDashLengths = [10, 10, 0]
-        llXAxis.labelPosition = .rightBottom
-        llXAxis.valueFont = .systemFont(ofSize: 10)
         chartView.xAxis.gridLineDashLengths = [10, 10]
         chartView.xAxis.gridLineDashPhase = 0
-        
-        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
-        ll1.lineWidth = 4
-        ll1.lineDashLengths = [10, 30]
-        ll1.labelPosition = .rightTop
-        ll1.valueFont = .systemFont(ofSize: 10)
-        
-        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
-        ll2.lineWidth = 4
-        ll2.lineDashLengths = [10,5]
-        ll2.labelPosition = .rightBottom
-        ll2.valueFont = .systemFont(ofSize: 10)
-        
+        chartView.xAxis.labelPosition = .bottom
+        //chartView.xAxis.valueFormatter = self
         let leftAxis = chartView.leftAxis
-        leftAxis.removeAllLimitLines()
-        //leftAxis.addLimitLine(ll1)
-        //leftAxis.addLimitLine(ll2)
-        leftAxis.axisMaximum = 200
-        leftAxis.axisMinimum = 0
-        leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.drawBottomYLabelEntryEnabled = true
+        leftAxis.drawTopYLabelEntryEnabled = true
+        leftAxis.gridLineDashLengths = [10, 10]
         leftAxis.drawLimitLinesBehindDataEnabled = true
         
         chartView.rightAxis.enabled = false
         
+        chartView.setScaleMinima(2.0, scaleY: 1)
         chartView.legend.form = .line
-        
+        let l = chartView.legend
+        l.wordWrapEnabled = true
+        l.horizontalAlignment = .center
+        l.verticalAlignment = .bottom
+        l.orientation = .horizontal
+        l.drawInside = false
         //chartView.animate(xAxisDuration: 2.5)
         
         setData()
     }
     
     fileprivate func setData() {
-        let count = 50
+        let count = 100
         let range = ClosedRange<Double>(uncheckedBounds: (0, 100))
         let values = (0..<count).map { (i) -> ChartDataEntry in
             let val = Double.random(in: range) + 3
@@ -80,6 +65,7 @@ class ViewController: UIViewController {
         //set1.lineDashLengths = [5, 2.5]
         set1.highlightLineDashLengths = [5, 2.5]
         set1.setColor(.black)
+        set1.axisDependency = .left
         set1.setCircleColor(.black)
         set1.lineWidth = 1
         set1.circleRadius = 3
@@ -96,10 +82,17 @@ class ViewController: UIViewController {
         set1.fillAlpha = 1
         set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
         set1.drawFilledEnabled = true
-        
         let data = LineChartData(dataSet: set1)
-        
+        chartView.xAxis.axisMaximum = data.xMax + 1
         chartView.data = data
+    }
+    
+}
+
+extension ViewController: IAxisValueFormatter {
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return String(value)
     }
     
 }
