9d184860f8ad1f742aaef4381d3e7b5d0d06e487
Aleksander Angelov <aleksander@kalipark.com>
2018-10-26 18:24:38 +0300

    applied some changes

--- ChartsDemo/ViewController.swift
+++ ChartsDemo/ViewController.swift
@@ -23,22 +23,38 @@ class ViewController: UIViewController {
         chartView.dragEnabled = true
         chartView.scaleXEnabled = true
         chartView.pinchZoomEnabled = true
-        chartView.gridBackgroundColor = UIColor.blue
         chartView.doubleTapToZoomEnabled = false
         chartView.highlightPerTapEnabled = true
-        chartView.xAxis.gridLineDashLengths = [10, 10]
-        chartView.xAxis.gridLineDashPhase = 0
-        chartView.xAxis.labelPosition = .bottom
+        let xAxis = chartView.xAxis
+        xAxis.gridLineDashLengths = [10, 10]
+        xAxis.gridLineDashPhase = 0
+        xAxis.labelPosition = .bottom
+        xAxis.axisMinimum = 0.0
+        
+        chartView.drawGridBackgroundEnabled = true
+        chartView.gridBackgroundColor = UIColor(red: 230.0/255.0, green: 242.0/255.0, blue: 250.0/255.0, alpha: 1.0)
         //chartView.xAxis.valueFormatter = self
         let leftAxis = chartView.leftAxis
         leftAxis.drawBottomYLabelEntryEnabled = true
         leftAxis.drawTopYLabelEntryEnabled = true
         leftAxis.gridLineDashLengths = [10, 10]
         leftAxis.drawLimitLinesBehindDataEnabled = true
-        
+        leftAxis.axisMinimum = 0.0
+        leftAxis.labelPosition = .insideChart
         chartView.rightAxis.enabled = false
         
+        let limitLine = ChartLimitLine(limit: 25, label: "First limit")
+        let limitLine2 = ChartLimitLine(limit: 50, label: "Second limit")
+        let limitLine3 = ChartLimitLine(limit: 75, label: "First limit")
+        limitLine.labelPosition = .leftTop
+        limitLine2.labelPosition = .leftTop
+        limitLine3.labelPosition = .leftTop
+        xAxis.addLimitLine(limitLine)
+        xAxis.addLimitLine(limitLine2)
+        xAxis.addLimitLine(limitLine3)
+        
         chartView.setScaleMinima(2.0, scaleY: 1)
+        chartView.scaleYEnabled = false
         chartView.legend.form = .line
         let l = chartView.legend
         l.wordWrapEnabled = true
@@ -64,9 +80,9 @@ class ViewController: UIViewController {
         
         //set1.lineDashLengths = [5, 2.5]
         set1.highlightLineDashLengths = [5, 2.5]
-        set1.setColor(.black)
+        set1.setColor(.gray)
         set1.axisDependency = .left
-        set1.setCircleColor(.black)
+        set1.setCircleColor(.gray)
         set1.lineWidth = 1
         set1.circleRadius = 3
         set1.drawCircleHoleEnabled = false
@@ -81,7 +97,7 @@ class ViewController: UIViewController {
         
         set1.fillAlpha = 1
         set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
-        set1.drawFilledEnabled = true
+        //set1.drawFilledEnabled = true
         let data = LineChartData(dataSet: set1)
         chartView.xAxis.axisMaximum = data.xMax + 1
         chartView.data = data
